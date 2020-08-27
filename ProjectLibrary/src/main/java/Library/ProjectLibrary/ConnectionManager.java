1b86795963aeb92f9a39e43cb00d739a91494f6d
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-08-27 11:09:18 -0500

    Set up the make connection portion and purposefully omitted the url/user/pass for later config file.

--- ProjectLibrary/src/main/java/Library/ProjectLibrary/ConnectionManager.java
+++ ProjectLibrary/src/main/java/Library/ProjectLibrary/ConnectionManager.java
@@ -7,7 +7,18 @@ public class ConnectionManager {
 	private static final String user = "";
 	private static final String pass = "";
 	
-	
+	private static void makeConnection() {
+		
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				connection = DriverManager.getConnection(url, user, pass);
+			} catch (SQLException | ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Could not make connection");
+			}
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
