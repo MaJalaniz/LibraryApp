886eb97ff68e9a488df2ac7b82112573bf122672
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-08-27 11:58:43 -0500

    Working on the Config.properties file and Connection Manager

--- ProjectLibrary/src/main/java/Library/ProjectLibrary/ConnectionManager.java
+++ ProjectLibrary/src/main/java/Library/ProjectLibrary/ConnectionManager.java
@@ -1,14 +1,29 @@
 package Library.ProjectLibrary;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 public class ConnectionManager {
 	private static Connection connection = null;
 	
-	private static final String url = "";
-	private static final String user = "";
-	private static final String pass = "";
-	
 	private static void makeConnection() {
+		Properties props = new Properties();
 		
+		try {
+			props.load(new FileInputStream("Resources/config.properties"));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		 String url = props.getProperty("url");
+		 String user = props.getProperty("user");
+		 String pass = props.getProperty("pass");
+		 
 			try {
 				Class.forName("com.mysql.cj.jdbc.Driver");
 				connection = DriverManager.getConnection(url, user, pass);
