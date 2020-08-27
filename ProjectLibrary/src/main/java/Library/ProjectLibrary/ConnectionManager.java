c7373a362952022b8450c5ee3e1fa29b47b0a49a
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-08-27 11:01:19 -0500

    Setting up the Connection Manager for JDBC

---
+++ ProjectLibrary/src/main/java/Library/ProjectLibrary/ConnectionManager.java
@@ -0,0 +1,16 @@
+package Library.ProjectLibrary;
+import java.sql.*;
+public class ConnectionManager {
+	private static Connection connection = null;
+	
+	private static final String url = "";
+	private static final String user = "";
+	private static final String pass = "";
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
