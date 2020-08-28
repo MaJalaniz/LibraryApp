c7c6d5cf3b4f0bcdbdae37f60f7551f56e2027da
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-08-28 09:48:59 -0500

    Set up the book model

--- ProjectLibrary/src/main/java/Library/ProjectLibrary/Book.java
+++ ProjectLibrary/src/main/java/Library/ProjectLibrary/Book.java
@@ -1,6 +1,74 @@
 package Library.ProjectLibrary;
 
+import java.time.LocalDate;
+
 public class Book {
+		private String isbn;
+		private String title;
+		private String description;
+		private int rented;
+		private LocalDate date;
+		
+		
+	public Book(String isbn, String title, String description, int rented, LocalDate date) {
+			super();
+			this.isbn = isbn;
+			this.title = title;
+			this.description = description;
+			this.rented = rented;
+			this.date = date;
+		}
+
+
+	public String getIsbn() {
+			return isbn;
+		}
+
+
+		public void setIsbn(String isbn) {
+			this.isbn = isbn;
+		}
+
+
+		public String getTitle() {
+			return title;
+		}
+
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+
+		public String getDescription() {
+			return description;
+		}
+
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+
+		public int getRented() {
+			return rented;
+		}
+
+
+		public void setRented(int rented) {
+			this.rented = rented;
+		}
+
+
+		public LocalDate getDate() {
+			return date;
+		}
+
+
+		public void setDate(LocalDate date) {
+			this.date = date;
+		}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
