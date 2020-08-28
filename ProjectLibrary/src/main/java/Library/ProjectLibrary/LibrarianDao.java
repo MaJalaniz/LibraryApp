f0c87c99cbc16d5f538a388398f9d7b338710794
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-08-28 10:10:21 -0500

    finished Librarian dao

---
+++ ProjectLibrary/src/main/java/Library/ProjectLibrary/LibrarianDao.java
@@ -0,0 +1,32 @@
+package Library.ProjectLibrary;
+
+public class LibrarianDao {
+	public boolean addBook(Book book) {
+		return false;
+	}
+	
+	public boolean deleteBook(int id) {
+		return false;
+	}
+	
+	public boolean updateBook(int id) {
+		return false;
+	}
+	
+	public boolean approveAccount(int id) {
+		return false;
+	}
+	public boolean freezeAccount(int id) {
+		return false;
+	}
+	
+	public boolean updateUsername(String newUserName) {
+		return false;
+	}
+	
+	public boolean updatePassword(String newPassword) {
+		return false;
+	}
+	
+
+}
