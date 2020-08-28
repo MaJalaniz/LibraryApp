2c35b97234e32391a01f0c181a24cd3f1b85b499
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-08-28 10:25:26 -0500

    working on patron dao

--- ProjectLibrary/src/main/java/Library/ProjectLibrary/PatronDao.java
+++ ProjectLibrary/src/main/java/Library/ProjectLibrary/PatronDao.java
@@ -3,5 +3,31 @@ import java.sql.*;
 
 public class PatronDao {
 	public static final Connection connection = ConnectionManager.getConnection();
+	private static final Patron pdao = null;
+	
+	public boolean checkoutBook(String isbn) {
+		return false;
+	}
+	
+	public boolean returnBook(String isbn) {
+		return false;
+	}
+	
+	public boolean createAccount() {
+		return false;
+	}
+	
+	public Patron login(String username, String password) {
+		return pdao;
+	}
+	
+	public boolean updateUsername(String newUsername) {
+		return false;
+	}
+	
+	public boolean updatePassword(String newPassword) {
+		return false;
+	}
+	
 	
 }
