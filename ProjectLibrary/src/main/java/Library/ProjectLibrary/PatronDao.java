df612b4a599218f76ba40833c801ab7faa85fd4f
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-08-28 12:04:24 -0500

    getting all books and getting all available books

--- ProjectLibrary/src/main/java/Library/ProjectLibrary/PatronDao.java
+++ ProjectLibrary/src/main/java/Library/ProjectLibrary/PatronDao.java
@@ -1,5 +1,8 @@
 package Library.ProjectLibrary;
 import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PatronDao {
 	public static final Connection connection = ConnectionManager.getConnection();
@@ -29,5 +32,30 @@ public class PatronDao {
 		return false;
 	}
 	
+	public List<Book> getAllBooks(){
+		List<Book> allBooks = new ArrayList<>();
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement("select * from book");
+			
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				Book book = new Book(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), LocalDate.parse(rs.getString(5)));
+				allBooks.add(book);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return allBooks;
+	}
 	
+	public List<Book> getAvailableBooks(){
+		List<Book> availableBooks = new ArrayList<>();
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement("select * from book where rented = 0");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next)
+		}
+	}
 }
