5c25803111553d4cea5ee5af08ecba5737d4e365
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-08-30 21:09:42 -0500

    Changed imported package to java util list not com dot sun

--- ProjectLibrary/src/main/java/Library/ProjectLibrary/AllBooksServlet.java
+++ ProjectLibrary/src/main/java/Library/ProjectLibrary/AllBooksServlet.java
@@ -1,5 +1,32 @@
 package Library.ProjectLibrary;
 
-public class AllBooksServlet {
+import java.util.List;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AllBooksServlet extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+		private PatronDao pdao;
+		
+	public void init(ServletConfig config) throws ServletException{
+		pdao = new PatronDao();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Book> allBooks = pdao.getAllBooks();
+		request.setAttribute("allBooks", allBooks);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("allBooks.jsp");
+			dispatcher.forward(request,  response);
+	}
+	
+	
 
 }
